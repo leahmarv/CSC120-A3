e92058a4930576efd4c2d12027d051f1cd762010
leahmarv <lmarville@smith.edu>
2022-09-24 21:59:07 -0400

    Added arrays for bot and user responses corresponding to number of rounds

--- Conversation.java
+++ Conversation.java
@@ -12,16 +12,32 @@ class Conversation {
 
     int rounds = myScanner.nextInt();
 
-    System.out.println("Hi! What's up?");
+    //System.out.println("Hi! What's up?");
 
-    String userLog[] = {};
-    String botLog[] = {};
+    String userLog[] = new String[rounds];
+    String botLog[] = new String[rounds];
     String randResponse[] = {"How about that!", "Whoa, I didn't know that.", "Sounds cool!"};
     
     int i = 0;
-    while(i<rounds) {
-      System.out.println(randResponse[random.nextInt(randResponse.length)]);
+    while(i < rounds) {
+      userLog[i] = myScanner.nextLine();
+      botLog[i] = randResponse[random.nextInt(randResponse.length)];
+      //scanner input should happen here
+      System.out.println(botLog[i]);
+      System.out.println("done");
       i++;
+      //scanner input is happening here
     }
+
+    //Prints chat log
+    System.out.println("TRANSCRIPT:");
+    int c = 0;
+    while(c < rounds) {
+      System.out.println(botLog[c]);
+      System.out.println(userLog[c]);
+      c++;
+    }
+
+
   }
 }
